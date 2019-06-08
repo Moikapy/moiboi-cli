2dac291e41518f82839ef9e020a72c52447b5375
Warren Gates <warrenckhan@gmail.com>
2019-06-08 16:05:40 -0400

    modified remove command to be used as Import statement

--- src/commands/index.js
+++ src/commands/index.js
@@ -3,13 +3,14 @@ import authorsCommand from './authors'
 import cloneCommand from './clone';
 import homeCommand from './home';
 import listCommand from './list'
+import removeCommand from './remove'
 export default {
   add: addCommand,
   authors: authorsCommand,
   clone: cloneCommand,
   home: homeCommand,
   list: listCommand,
-  remove: require('./remove'),
+  remove: removeCommand,
   reset: require('./reset'),
   search: require('./search'),
 }
